8fac4d1b58e607726dd8a0d06d8f64838f20687e
Talha Hussain <theuntitled95@gmail.com>
2025-05-19 13:24:40 +0400

    uni-prep init

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,12 @@
+import {useTranslations} from "next-intl";
+
+export default function Home() {
+  const t = useTranslations("Homepage");
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>{t("title")}</h1>
+      <p>{t("description")}</p>
+    </div>
+  );
+}
